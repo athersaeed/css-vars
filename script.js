8c75ec8a14f0c12c10aecf2f2f31432e57d77fc3
athersaeed <athers896@gmail.com>
2025-09-18 18:41:40 -0400

    played with a few CSS Vars and manipulated them using JS

---
+++ script.js
@@ -0,0 +1,9 @@
+const inputs = document.querySelectorAll('.controls input')
+
+function changeHandler(){
+    const suffix = this.dataset.sizing || ''
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix)
+}
+
+inputs.forEach(input => input.addEventListener('mousemove', changeHandler))
+inputs.forEach(input => input.addEventListener('change', changeHandler))
